d3e75538f533e74dd27ca7ae2307f3025f012c7d
Alex <aaermolov@gmail.com>
2014-09-10 17:28:22 +0400

    reformatted comments a bit

--- rc.lua
+++ rc.lua
@@ -3,12 +3,9 @@ local gears = require("gears")
 local awful = require("awful")
 awful.rules = require("awful.rules")
 require("awful.autofocus")
--- Widget and layout library
-local wibox = require("wibox")
--- Theme handling library
-local beautiful = require("beautiful")
--- Notification library
-local naughty = require("naughty")
+local wibox = require("wibox") -- Widget and layout library
+local beautiful = require("beautiful") -- Theme handling library
+local naughty = require("naughty") -- Notification library
 local menubar = require("menubar")
 local vicious = require("vicious")
 

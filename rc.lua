bbf31fdb27966e6ed7491e012c3162471482cb2f
Alex <aaermolov@gmail.com>
2014-09-10 12:41:54 +0400

    removed unwanted layouts

--- rc.lua
+++ rc.lua
@@ -74,9 +74,7 @@ local layouts =
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral,
     awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier
+    awful.layout.suit.max
 }
 -- }}}
 

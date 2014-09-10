1fa79dde26d44841b43d0973e3ce093bf8902821
Alex <aaermolov@gmail.com>
2014-09-10 17:33:34 +0400

    removed a bunch of unneeded comments

--- rc.lua
+++ rc.lua
@@ -114,17 +114,13 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 -- }}}
 
 -- {{{ Wibox
--- Create a textclock widget
 mytextclock = awful.widget.textclock()
 
--- Инициализация виджета
 cpuwidget = awful.widget.graph()
--- Свойства графика
 cpuwidget:set_width(50)
 cpuwidget:set_background_color("#494B4F")
 cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"}, 
                     {1, "#AECF96" }}})
--- Регистрация виджета
 vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
 
 batwidget = awful.widget.progressbar()
@@ -137,25 +133,20 @@ batwidget:set_color("#AECF96")
 vicious.register(batwidget, vicious.widgets.bat, "$2", 61, "BAT0")
 
 memwidget = awful.widget.progressbar()
--- Progressbar properties
 memwidget:set_width(8)
 memwidget:set_height(10)
 memwidget:set_vertical(true)
 memwidget:set_background_color("#494B4F")
 memwidget:set_border_color(nil)
 memwidget:set_color("#AECF96")
--- Register widget
 vicious.register(memwidget, vicious.widgets.mem, "$1", 13)
 
--- Keyboard layout widget
 kbdwidget = wibox.widget.textbox(" Eng ")
 kbdwidget.border_width = 1
 kbdwidget.border_color = beautiful.fg_normal
 kbdwidget:set_text(" Eng ")
-
 kbdstrings = {[0] = " Eng ", 
               [1] = " Рус "}
-
 dbus.request_name("session", "ru.gentoo.kbdd")
 dbus.add_match("session", "interface='ru.gentoo.kbdd',member='layoutChanged'")
 dbus.connect_signal("ru.gentoo.kbdd", function(...)
@@ -165,9 +156,7 @@ dbus.connect_signal("ru.gentoo.kbdd", function(...)
     end
 )
 
--- Initialize widget
 mpdwidget = wibox.widget.textbox()
--- Register widget
 vicious.register(mpdwidget, vicious.widgets.mpd,
     function (mpdwidget, args)
         if args["{state}"] == "Stop" then 

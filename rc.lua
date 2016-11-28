19390190a0a0b5e33a84ffdfb7cb2c0bd742b1b9
Alex Ermolov <aermolov@mirantis.com>
2016-11-28 12:22:28 +0300

    removed extra comment

--- rc.lua
+++ rc.lua
@@ -53,11 +53,6 @@ editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 browser = "firefox -new-tab"
 
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.

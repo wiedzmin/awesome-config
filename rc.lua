c97e56dc571c05b60179bac6de08fd8a71208eb6
Alex Ermolov <aermolov@mirantis.com>
2016-11-30 21:15:04 +0300

    removed dead code

--- rc.lua
+++ rc.lua
@@ -562,4 +562,3 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
--- awful.util.spawn("xxkb &")

c28242d65a6ef1175fab39384b2f4a32b1f697d4
Alex Ermolov <aermolov@mirantis.com>
2016-11-30 20:57:34 +0300

    removed forgotten code that was moved to separate module

--- rc.lua
+++ rc.lua
@@ -124,68 +124,6 @@ mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
 
--- {{{ Wibox
-mytextclock = awful.widget.textclock()
-
-cpuwidget = awful.widget.graph()
-cpuwidget:set_width(50)
-cpuwidget:set_background_color("#494B4F")
-cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"},
-                    {1, "#AECF96" }}})
-vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
-
-batwidget = awful.widget.progressbar()
-batwidget:set_width(8)
-batwidget:set_height(10)
-batwidget:set_vertical(true)
-batwidget:set_background_color("#494B4F")
-batwidget:set_border_color(nil)
-batwidget:set_color("#AECF96")
-vicious.register(batwidget, vicious.widgets.bat, "$2", 61, "BAT0")
-
-memwidget = awful.widget.progressbar()
-memwidget:set_width(8)
-memwidget:set_height(10)
-memwidget:set_vertical(true)
-memwidget:set_background_color("#494B4F")
-memwidget:set_border_color(nil)
-memwidget:set_color("#AECF96")
-vicious.register(memwidget, vicious.widgets.mem, "$1", 13)
-
-kbdwidget = wibox.widget.textbox(" Eng ")
-kbdwidget.border_width = 1
-kbdwidget.border_color = beautiful.fg_normal
-kbdwidget:set_text(" Eng ")
-kbdstrings = {[0] = " Eng ",
-              [1] = " Рус "}
-dbus.request_name("session", "ru.gentoo.kbdd")
-dbus.add_match("session", "interface='ru.gentoo.kbdd',member='layoutChanged'")
-dbus.connect_signal("ru.gentoo.kbdd", function(...)
-    local data = {...}
-    local layout = data[2]
-    kbdwidget:set_markup(kbdstrings[layout])
-    end
-)
-
-mpdwidget = wibox.widget.textbox()
-vicious.register(mpdwidget, vicious.widgets.mpd,
-    function (mpdwidget, args)
-        if args["{state}"] == "Stop" then
-            return " - "
-        else
-            return args["{Artist}"]..' - '.. args["{Title}"]
-        end
-    end, 10)
-
-
--- TODO maybe add gmail icon
-gmailicon = wibox.widget.imagebox()
-gmailicon:set_image("/home/username/.config/awesome/gmail.png")
-gmailwidget = wibox.widget.textbox()
-vicious.register(gmailwidget, vicious.widgets.gmail, "GMail: ${count}", 260)
-
-separator = wibox.widget.textbox()
-separator:set_text(" :: ")
 
 -- Create a wibox for each screen and add it
 mywibox = {}
@@ -267,7 +205,6 @@ for s = 1, screen.count() do
     local right_layout = wibox.layout.fixed.horizontal()
     right_layout:add(mpdwidget)
     right_layout:add(separator)
-    right_layout:add(gmailwidget)
     right_layout:add(separator)
     right_layout:add(memwidget)
     right_layout:add(separator)

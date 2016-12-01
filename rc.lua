63ba950306929b553ed5fdc8908813f0542387c3
Alex Ermolov <aermolov@mirantis.com>
2016-12-01 22:06:20 +0300

    removed nonfunctional MPD widget from wibox

--- rc.lua
+++ rc.lua
@@ -200,8 +200,8 @@ for s = 1, screen.count() do
 
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
-    right_layout:add(mpdwidget)
-    right_layout:add(separator)
+    -- right_layout:add(mpdwidget)
+    -- right_layout:add(separator)
     right_layout:add(separator)
     right_layout:add(memwidget)
     right_layout:add(separator)

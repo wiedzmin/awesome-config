e81ba62345e8830ad6ef07956777757ea3ec8e1e
Alex Ermolov <aermolov@mirantis.com>
2016-12-03 21:30:02 +0300

    rewritten global keybindings in "ez" notation

--- rc.lua
+++ rc.lua
@@ -15,6 +15,7 @@ hints = require("hints")
 lain = require("lain")
 eminent = require("eminent")
 cheeky = require("cheeky")
+ezconfig = require("ezconfig")
 
 local gears = require("gears")
 local naughty = require("naughty") -- Notification library
@@ -315,101 +316,80 @@ function websearches_map()
 end
 -- }}}
 
--- {{{ Key bindings
-globalkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
+ezconfig.modkey = "Mod4"
+ezconfig.altkey = "Mod1"
 
-    awful.key({ modkey,           }, "Left",   function () awful.client.focus.global_bydirection('left')    end),
-    awful.key({ modkey,           }, "Right",  function () awful.client.focus.global_bydirection('right')    end),
-    awful.key({ modkey,           }, "Up",     function () awful.client.focus.global_bydirection('up')    end),
-    awful.key({ modkey,           }, "Down",   function () awful.client.focus.global_bydirection('down')    end),
-
-
-    awful.key({ modkey,           }, "j",
-        function ()
-            awful.client.focus.byidx( 1)
-            if client.focus then client.focus:raise() end
-        end),
-    awful.key({ modkey,           }, "k",
-        function ()
-            awful.client.focus.byidx(-1)
-            if client.focus then client.focus:raise() end
-        end),
-    -- awful.key({ modkey,           }, "w", function () mymainmenu:show() end),
-
-    -- Layout manipulation
-    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
-    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
-    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
-    awful.key({ modkey, "Shift"   }, ",", function () awful.screen.focus(1) end),
-    awful.key({ modkey, "Shift"   }, ".", function () awful.screen.focus(2) end),
-    awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
-    awful.key({ modkey,           }, "Tab",
+globalkeys = ezconfig.keytable.join({
+    ['M-<Escape>'] = awful.tag.history.restore,
+    ['M-<Left>'] = function () awful.client.focus.global_bydirection('left') end,
+    ['M-<Right>'] = function () awful.client.focus.global_bydirection('right') end,
+    ['M-<Up>'] = function () awful.client.focus.global_bydirection('up') end,
+    ['M-<Down>'] = function () awful.client.focus.global_bydirection('down') end,
+    ['M-j'] = function ()
+        awful.client.focus.byidx( 1)
+        if client.focus then client.focus:raise() end
+        end,
+    ['M-k'] = function ()
+        awful.client.focus.byidx(-1)
+        if client.focus then client.focus:raise() end
+        end,
+    ['M-S-j'] = function () awful.client.swap.byidx(  1) end,
+    ['M-S-k'] = function () awful.client.swap.byidx( -1) end,
+    ['M-C-j'] = function () awful.screen.focus_relative( 1) end,
+    ['M-C-k'] = function () awful.screen.focus_relative(-1) end,
+    ['M-S-,'] = function () awful.screen.focus(1) end,
+    ['M-S-.'] = function () awful.screen.focus(2) end,
+    ['M-u'] = awful.client.urgent.jumpto,
+    ['M-<Tab>'] =
         function ()
             awful.client.focus.history.previous()
             if client.focus then
                 client.focus:raise()
             end
-        end),
-
-    -- Standard program
-    awful.key({ modkey,           }, "Return", function () awful.util.spawn(defs.terminal) end),
-    awful.key({ modkey, "Control" }, "r", awesome.restart),
-    awful.key({ modkey, "Shift"   }, "q", awesome.quit),
-
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
-    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-    awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
-    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
-
-    awful.key({ modkey, "Control" }, "n", awful.client.restore),
-
-    -- Prompt
-    -- awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
-
-    awful.key({ modkey }, "x",
+        end,
+    ['M-Return'] = function () awful.util.spawn(defs.terminal) end,
+    ['M-C-r'] = awesome.restart,
+    ['M-S-q'] = awesome.quit,
+    ['M-l'] = function () awful.tag.incmwfact(0.05) end,
+    ['M-h'] = function () awful.tag.incmwfact(-0.05) end,
+    ['M-S-h'] = function () awful.tag.incnmaster(1) end,
+    ['M-S-l'] = function () awful.tag.incnmaster(-1) end,
+    ['M-C-h'] = function () awful.tag.incncol(1) end,
+    ['M-C-l'] = function () awful.tag.incncol(-1) end,
+    ['M-<Space>'] = function () awful.layout.inc(layouts, 1) end,
+    ['M-S-<Space>'] = function () awful.layout.inc(layouts, -1) end,
+    ['M-C-n'] = awful.client.restore,
+    ['M-x'] =
               function ()
                   awful.prompt.run({ prompt = "Run Lua code: " },
                   mypromptbox[mouse.screen].widget,
                   awful.util.eval, nil,
                   awful.util.getdir("cache") .. "/history_eval")
-              end),
-    -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end),
-
-    awful.key({ modkey, "Control" }, "z", function () awful.util.spawn("mpc prev") end),
-    awful.key({ modkey, "Control" }, "x", function () awful.util.spawn("mpc play") end),
-    awful.key({ modkey, "Control" }, "c", function () awful.util.spawn("mpc toggle") end),
-    awful.key({ modkey, "Control" }, "v", function () awful.util.spawn("mpc stop") end),
-    awful.key({ modkey, "Control" }, "b", function () awful.util.spawn("mpc next") end),
-
-    awful.key({ modkey }, "Escape", function() show_apps_menu() end),
-
-    awful.key({}, "XF86AudioRaiseVolume", function () awful.util.spawn("amixer -c 0 set Master 10+") end),
-    awful.key({}, "XF86AudioLowerVolume", function () awful.util.spawn("amixer -c 0 set Master 10-") end),
-    awful.key({}, "XF86AudioMute", function () awful.util.spawn("amixer set Master toggle >> /dev/null") end),
-
-    awful.key({ modkey, "Control" }, "l", function () awful.util.spawn("i3lock -c 232729") end), -- TODO: fix "xset dpms force off"
-
-    awful.key({ modkey, "Shift" }, "Right", function () awful.util.spawn("xrandr --output VGA1 --auto --right-of LVDS1") end),
-    awful.key({ modkey, "Shift" }, "Left", function () awful.util.spawn("xrandr --output VGA1 --auto --left-of LVDS1") end),
-    awful.key({ modkey, "Shift" }, "Up", function () awful.util.spawn("xrandr --output VGA1 --auto --above LVDS1") end),
-    awful.key({ modkey, "Shift" }, "Down", function () awful.util.spawn("xrandr --output VGA1 --off") end),
-
-    awful.key({ modkey }, "2", function () run_or_raise_map() end),
-    awful.key({ modkey }, "w", function () webjumps_map() end),
-    awful.key({ modkey }, "/", function () websearches_map() end),
-    awful.key({ "Control" }, "\\", function () os.execute(defs.kbdd_dbus_prev_cmd) end),
-    awful.key({ modkey }, "e", function () hints.focus() end),
-    awful.key({ modkey, "Shift" }, "p", function () awful.util.spawn("gmrun") end),
-    awful.key({ modkey, "Shift" }, "/", function() cheeky.util.switcher() end),
-    awful.key({ }, "Print", function () awful.util.spawn("scrot -e 'mv $f ~/screenshots/ 2>/dev/null'") end) -- TODO: take screenshot command from stumpwm
-)
+              end,
+    ['M-p'] = function() menubar.show() end,
+    ['M-C-z'] = function () awful.util.spawn("mpc prev") end,
+    ['M-C-x'] = function () awful.util.spawn("mpc play") end,
+    ['M-C-c'] = function () awful.util.spawn("mpc toggle") end,
+    ['M-C-v'] = function () awful.util.spawn("mpc stop") end,
+    ['M-C-b'] = function () awful.util.spawn("mpc next") end,
+    ['M-<Escape>'] = function() show_apps_menu() end,
+    ['XF86AudioRaiseVolume'] = function () awful.util.spawn("amixer -c 0 set Master 10+") end,
+    ['XF86AudioLowerVolume'] = function () awful.util.spawn("amixer -c 0 set Master 10-") end,
+    ['XF86AudioMute'] = function () awful.util.spawn("amixer set Master toggle >> /dev/null") end,
+    ['M-C-l'] = function () awful.util.spawn("i3lock -c 232729") end, -- TODO: fix "xset dpms force off"
+    ['M-S-<Right>'] = function () awful.util.spawn("xrandr --output VGA1 --auto --right-of LVDS1") end,
+    ['M-S-<Left>'] = function () awful.util.spawn("xrandr --output VGA1 --auto --left-of LVDS1") end,
+    ['M-S-<Up>'] = function () awful.util.spawn("xrandr --output VGA1 --auto --above LVDS1") end,
+    ['M-S-<Down>'] = function () awful.util.spawn("xrandr --output VGA1 --off") end,
+    ['M-2'] = function () run_or_raise_map() end,
+    ['M-w'] = function () webjumps_map() end,
+    ['M-/'] = function () websearches_map() end,
+    ['C-\\'] = function () os.execute(defs.kbdd_dbus_prev_cmd) end,
+    ['M-e'] = function () hints.focus() end,
+    ['M-S-p'] = function () awful.util.spawn("gmrun") end,
+    ['M-S-/'] = function() cheeky.util.switcher() end,
+    ['Print'] = function () awful.util.spawn("scrot -e 'mv $f ~/screenshots/ 2>/dev/null'") end -- TODO: take screenshot command from stumpwm
+})
 
 clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
